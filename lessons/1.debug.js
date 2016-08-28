9dd2a79023eb6a98a94b75cbe30916ec88ca436b
943278759@qq.com <dimdragon81708>
2016-08-28 17:09:07 +0800

    最终

--- lessons/1.debug.js
+++ lessons/1.debug.js
@@ -1,10 +1,9 @@
-//可用于一些调试信息
+//用于用打印一些调试信息
 var debug = require('./debug');
-//console.log(process.env.DEBUG)
-var loggerServer = debug('logger:server ');
+//传入一个名字可以得到对应的日志记录器
+var loggerServer = debug('logger:server');
 //调用此函数在控制台输出日志
-//输出时，会用此日志记录器的名字和环境变量中的DEBUG值进行比较。如相同，
-loggerServer('server')
-var loggerClient = debug('logger:client ');
-
-loggerClient("client")
+// 输出的时候，会用此日志记录器的名字和环境变量中的DEUBG值进行比较，如果相同，则输出到控制台，如果不相同，则不输出任何东西
+loggerServer('server');
+var loggerClient = debug('logger:client');
+loggerClient('client');
--- lessons/debug.js
+++ lessons/debug.js
@@ -1,17 +1,16 @@
-
-module.exports=function(name){
+module.exports = function(name){
     /**
-     * 读取环境变量中的DEBUG值，如果和自己名字一样，则输出msg
+     * 读取环境变量中的DEBUG值，如果和自己的名字一样，则输出msg
      * 如果不一样，什么都不做
      */
-    return function(msg){
-        //
-        var debug=process.env.DEBUG;
-        debug='^'+debug.replace('*','.*');
-        var regex=new RegExp(debug);
-        if(regex.test(name)){
-            console.log(name,msg)
-        }
-
-    }
+   return function(msg){
+       //先获取环境变量中的debug值
+      var debug = process.env.DEBUG;
+       //进行字符串替换
+      debug = '^'+debug.replace('*','.*');
+      var regex = new RegExp(debug);
+       //  ^logger:.*
+      if(regex.test(name))
+        console.log(name,msg);
+   }
 }
